a897a03032b1d4c6e1b07d48a2944a4635094511
sagarmk <blacksaint@saintsnest>
2023-04-15 03:01:17 -0600

    init

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# qt_graphviz
+# Graphviz Flowchart Tool
+
+Graphviz Flowchart Tool is a simple and user-friendly application for creating flowcharts using Graphviz's DOT language. The application allows you to write DOT code, generate an SVG flowchart from the code, and save or copy the SVG to your clipboard.
+
+![Screenshot of the Graphviz Flowchart Tool](qt_graphviz.png)
+
+## Installation
+
+1. Ensure you have [Python 3](https://www.python.org/downloads/) installed on your system.
+2. Install Graphviz, if you haven't already, using the instructions for your operating system from the [Graphviz website](https://graphviz.org/download/).
+3. Clone this repository or download the source code.
+4. Navigate to the repository or source code folder and install the required dependencies using the following command:
+
+"pip install -r requirements.txt"
+
+## Usage
+
+1. Run the `main.py` script:
+"python main.py"
+2. The Graphviz Flowchart Tool window will appear. Enter your DOT code into the left pane.
+3. Click the "Build Flowchart" button to generate the flowchart in the right pane.
+4. To copy the SVG code to your clipboard, click the "Copy SVG" button.
+5. To save the generated SVG as a file, click the "Save SVG" button and choose a location.
+
+## License
+
+This project is licensed under the MIT License. See the [LICENSE](LICENSE) file for more details.
---
+++ main.py
@@ -0,0 +1,112 @@
+from PyQt5.QtWidgets import (QApplication, QMainWindow, QLabel, QPlainTextEdit, QPushButton, QFileDialog,
+                             QGraphicsView, QGraphicsScene, QVBoxLayout, QHBoxLayout, QWidget, QSplitter)
+from PyQt5.QtGui import QClipboard, QPainter
+from PyQt5.QtCore import Qt, QProcess
+from PyQt5.QtSvg import QGraphicsSvgItem
+import os
+import tempfile  
+
+class MainWindow(QMainWindow):
+    """
+    Main window class that inherits from QMainWindow and handles the Graphviz flowchart tool's UI.
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        """
+        Initializes the UI components and their layout.
+        """
+
+        self.setWindowTitle("Graphviz Flowchart Tool")
+
+        # Set up DOT code editor and flowchart display
+        self.dot_editor = QPlainTextEdit()
+        self.flowchart_view = QGraphicsView()
+        self.flowchart_view.setRenderHint(QPainter.Antialiasing)
+
+        # Set up flowchart scene and splitter for resizing
+        self.flowchart_scene = QGraphicsScene(self)
+        self.flowchart_view.setScene(self.flowchart_scene)
+        splitter = QSplitter(Qt.Horizontal)
+        splitter.addWidget(self.dot_editor)
+        splitter.addWidget(self.flowchart_view)
+        splitter.setSizes([400, 400])
+
+        # Set up buttons and their layout
+        self.build_button = QPushButton("Build Flowchart")
+        self.build_button.clicked.connect(self.build_flowchart)
+        self.copy_button = QPushButton("Copy SVG")
+        self.copy_button.clicked.connect(self.copy_svg)
+        self.save_button = QPushButton("Save SVG")
+        self.save_button.clicked.connect(self.save_svg)
+        buttons_layout = QHBoxLayout()
+        buttons_layout.addWidget(self.build_button)
+        buttons_layout.addWidget(self.copy_button)
+        buttons_layout.addWidget(self.save_button)
+
+        # Set up main layout and central widget
+        main_layout = QVBoxLayout()
+        main_layout.addWidget(splitter)
+        main_layout.addLayout(buttons_layout)
+        central_widget = QWidget()
+        central_widget.setLayout(main_layout)
+        self.setCentralWidget(central_widget)
+
+    def build_flowchart(self):
+            """
+            Generates an SVG flowchart from the input DOT code using Graphviz.
+            """
+
+            dot_code = self.dot_editor.toPlainText()
+
+            # Write DOT code to a temporary file
+            with tempfile.NamedTemporaryFile('w', delete=False, suffix='.dot') as dot_temp:
+                dot_temp.write(dot_code)
+                dot_temp.flush()
+
+                # Generate SVG from DOT code using Graphviz
+                with tempfile.NamedTemporaryFile('w', delete=False, suffix='.svg') as svg_temp:
+                    process = QProcess(self)
+                    process.start("dot", ["-Tsvg", dot_temp.name, "-o", svg_temp.name])
+                    process.waitForFinished()
+
+                    # Display generated SVG in the QGraphicsView
+                    self.flowchart_scene.clear()
+                    svg_item = QGraphicsSvgItem(svg_temp.name)
+                    self.flowchart_scene.addItem(svg_item)
+                    self.flowchart_view.setSceneRect(svg_item.boundingRect())
+
+                    # Store the SVG file name for later use
+                    self.temp_svg_filename = svg_temp.name
+
+                # Clean up the temporary DOT file
+                os.unlink(dot_temp.name)
+
+    def copy_svg(self):
+        """
+        Copies the generated SVG code to the clipboard.
+        """
+
+        with open("temp.svg", "r") as f:
+            svg_code = f.read()
+
+        clipboard = QApplication.clipboard()
+        clipboard.setText(svg_code, QClipboard.Clipboard)
+
+    def save_svg(self):
+        """
+        Saves the generated SVG to a user-selected file.
+        """
+
+        file_name, _ = QFileDialog.getSaveFileName(self, "Save SVG", "", "SVG files (*.svg)")
+        if file_name:
+            os.rename("temp.svg", file_name)
+
+if __name__ == "__main__":
+    app = QApplication([])
+    window = MainWindow()
+    window.show()
+    app.exec_()
